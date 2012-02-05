51ccee0b23d22dd1c63df7be85ad8c12209485ad
nulltask <seiya@uniba.jp>
2012-02-05 22:48:10 +0900

    edge detection code
    inspired: http://shokai.org/blog/archives/5668

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -10,7 +10,6 @@
     
   $(function() {
     var video = document.getElementById('monitor')
-      , audio = document.getElementById('microphone')
       , canvas = document.getElementById('capture')
       , receive = document.getElementById('receive');
 
@@ -20,11 +19,11 @@
     });
 
     if (!getUserMedia) {
-      alert('your browser unsupported webrtc.');
+      // alert('your browser unsupported webrtc.');
       return false;
     }
         
-    navigator.webkitGetUserMedia('video', function(stream) {
+    navigator.webkitGetUserMedia('video audio', function(stream) {
       video.src = webkitURL.createObjectURL(stream);
       video.onerror = function () {
         stream.stop();
@@ -33,14 +32,43 @@
     
     $(video).on('timeupdate', function(e) {
       var ctx = canvas.getContext('2d')
-        , imageData;
+        , imageData, edge, quant = [];
       
       ctx.drawImage(video, 0, 0);
+      imageData = ctx.getImageData(0, 0, 320, 240);
+      
+      for (var i = 0, n = imageData.data.length; i < n; i += 4) {
+        var r = imageData.data[i] & 0xff
+          , g = imageData.data[i+1] & 0xff
+          , b = imageData.data[i+2] & 0xff
+          , gray = (r + g + b) / 3;
+        
+        quant.push(gray & 0xc0);
+      }
+      
+      edge = ctx.createImageData(320, 240);
+      
+      for (var y = 1; y < 240 - 1; ++y) {
+        for (var x = 1; x < 320 - 1; ++x) {
+          var i = y * 320 + x
+            , c
+            , around = (quant[i-320] + quant[i-1] + quant[i+1] + quant[i+320]) / 4;
+
+          if (around < quant[i]) {
+            c = 0;
+          } else {
+            c = 255;
+          }
+          
+          edge.data[i*4] = c;
+          edge.data[i*4+1] = c;
+          edge.data[i*4+2] = c;
+          edge.data[i*4+3] = 255;
+        }
+      }
+      
+      ctx.putImageData(edge, 0, 0);
       socket.emit('capture', canvas.toDataURL());
-      // imageData = ctx.getImageData(0, 0, 320, 240);
-      // for (var i = 0; i < imageData.length; ++i) {
-        // console.log(i);
-      // }
     });
     
   });
